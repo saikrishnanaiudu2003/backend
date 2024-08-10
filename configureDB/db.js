f5deb25ca651ae17e390a1a89bb7e579a26d97a3
krishnalasyatalluri <krishnalasyatalluri9@gmail.com>
2024-08-10 18:44:40 +0530

    added mongodb url

--- configureDB/db.js
+++ configureDB/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const configureDB=async(req,res)=>{
     try{
-        await mongoose.connect('mongodb://127.0.0.1/task-manager',
+        await mongoose.connect('mongodb+srv://krishnalasyatalluri9:Lasya0425@tasks.8tzjo.mongodb.net/?retryWrites=true&w=majority&appName=Tasks',
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
